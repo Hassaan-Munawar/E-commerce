f57b51ddfb75f6f445a1c54e063211a9e5c74294
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2025-07-16 21:03:54 +0500

    dark theme updated

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 export default function ThemeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
