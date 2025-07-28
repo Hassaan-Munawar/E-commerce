7f4e6dea86aff718297fe063fd9ede16ea9dba61
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2025-07-28 22:46:49 +0500

    commits

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import "dotenv/config"
+import cors from "cors";
+import connectDB from './config/connectDb.js';
+
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+
+connectDB();
+
 
 app.get('/', (req, res) => {
     res.send('Hi World!');
@@ -9,4 +17,4 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000, () => console.log("Server is running on PORT " + 3000));
+app.listen(process.env.PORT, () => console.log("Server is running on PORT " + process.env.PORT));
