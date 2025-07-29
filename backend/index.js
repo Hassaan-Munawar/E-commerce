4c862c737751e21e7f8ae990c7911e8131b33678
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2025-07-29 08:02:07 +0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.use("/api/products", productRoutes);
+app.use("/products", productRoutes);
 
 app.listen(process.env.PORT, () => console.log("Server is running on PORT " + process.env.PORT))
