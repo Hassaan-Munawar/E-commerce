ea367e2eae641b4c6bf15c08431793822c90b676
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2025-07-29 06:16:27 +0500

    backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use("/api/users", (req, res) => {
+    res.send("User API Endpoint");
+});
+
+app.listen(process.env.PORT, () => console.log("Server is running on PORT " + process.env.PORT))
