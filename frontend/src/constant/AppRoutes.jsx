3ac5fb0972de34cf97f191394b3165a0ab085799
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2025-07-31 14:35:55 +0500

    commits

--- frontend/src/constant/AppRoutes.jsx
+++ frontend/src/constant/AppRoutes.jsx
@@ -1,7 +1,7 @@
-const dev = 'http://localhost:5001/'
-const prod = 'https://h-e-commerce-backend.vercel.app/'
+const dev = process.env.DEV 
+const prod = process.env.PROD
 
-const BASE_URL = dev
+const BASE_URL = prod
 
 export const AppRoutes = {
     login: BASE_URL+"user",
